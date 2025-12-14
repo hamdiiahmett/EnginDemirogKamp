1861a5fc82b16e83feb7ad237a16afc1435be9db
Ahmet Hamdi Oğuz <ahoo00881@icloud.com>
2025-12-14 16:33:33 +0300

    gercek musteri ve tuzel musteri ayirdik idleri iki tarafa da yazdik

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -2,3 +2,16 @@
 //SOYUTLAMA 
 
 
+using OOP2;
+
+//Ahmet Hamdi 
+Musteri musteri1 = new Musteri();
+musteri1.Adi = "Ahmet";
+musteri1.Soyadi = "Hamdi";
+musteri1.Id = 1;
+musteri1.TcNo = "32133322233";
+musteri1.MusteriNo = "12345";
+//musteri1.SirketAdi = "?"; sirket adi benimle alakali bir sey degil aslinda
+
+//Gercek Musteri - Tuzel Musteri
+//SOLID 
---
+++ OOP2/TuzelMusteri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    //Coorporate
+    internal class TuzelMusteri
+    {
+        public int Id { get; set; } //ikisinde de var
+        public string MusteriNo { get; set; } //ikisinde de var
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+
+    }
+}
